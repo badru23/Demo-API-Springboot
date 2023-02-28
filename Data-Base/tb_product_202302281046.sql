b483d74cde05993d5446d7aa96d77fd3b8386adb
Muhamad Badru Kamal <muhamad.kamal@indivaragroup.com>
2023-02-28 10:49:06 +0700

    Data base

---
+++ Data-Base/tb_product_202302281046.sql
@@ -0,0 +1,5 @@
+INSERT INTO demoapi.tb_product (description_product,name_product,price_product) VALUES
+	 ('Keyboard Gaming','Keyboard',2000.0),
+	 ('Laptop Asus','Laptop',10000.0),
+	 ('Camera canon','Camera',5000.0),
+	 ('laptop Lenovo','Laptop',1000.0);
